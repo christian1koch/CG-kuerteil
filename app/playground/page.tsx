ae83344f30e1685ae373b8ea6045ab57354ed054
christian1koch <ko.che@web.de>
2025-12-01 10:51:24 +0100

    Hud first implementaiton

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -6,19 +6,26 @@ import {
     Environment,
     ContactShadows,
     Text,
-    useFont,
+    PerspectiveCamera,
+    Hud,
 } from "@react-three/drei";
 import { Suspense, useState } from "react";
 import { WorkExperienceText } from "../beleg/Texts";
 import { WORK_EXPERIENCE_MOCKS } from "../beleg/constants";
+import { Button } from "@react-three/uikit-default";
+import { Text as UiText, Container, Portal } from "@react-three/uikit";
+import { Mails, Smile } from "@react-three/uikit-lucide";
+import { Briefcase } from "../beleg/Briefcase";
+import { Statue } from "../beleg/Statue";
+import { Book } from "../beleg/Book";
+import { Envelope } from "../beleg/Envelope";
+import { MeshBasicMaterial } from "three";
 
 export default function Playground() {
-    const font = useFont("/geist-mono-regular-font.json");
     const [disableZoom, setDisableZoom] = useState(false);
     return (
-        <div className="h-screen w-full bg-black">
+        <div className="h-screen w-full">
             <Canvas
-                camera={{ position: [0, 2, 5], fov: 50 }}
                 gl={{
                     stencil: true,
                 }}
@@ -46,6 +53,55 @@ export default function Playground() {
                     <OrbitControls enableZoom={!disableZoom} />
                     <Environment preset="city" />
                 </Suspense>
+                <Hud>
+                    <ambientLight intensity={10} />
+                    <PerspectiveCamera makeDefault position={[2.5, -2.5, 6]} />
+                    <Container gap={20} flexDirection={"row"}>
+                        <Button variant="outline">
+                            <Container padding={0.2}>
+                                <UiText>About me</UiText>
+                                <Portal width={30} aspectRatio={1}>
+                                    <ambientLight intensity={2} />
+                                    <Statue
+                                        scale={0.05}
+                                        position={[0, -0.2, 0]}
+                                    />
+                                </Portal>
+                            </Container>
+                        </Button>
+                        <Button variant="outline">
+                            <Container padding={0.2} gap={5}>
+                                <UiText>Work Experience</UiText>{" "}
+                                <Portal width={50} aspectRatio={1}>
+                                    <ambientLight intensity={10} />
+                                    <Briefcase
+                                        scale={2}
+                                        position={[0, -1, 0]}
+                                    />
+                                </Portal>
+                            </Container>
+                        </Button>
+                        <Button variant="outline">
+                            <Container padding={0.2} gap={5}>
+                                <UiText>Education</UiText>{" "}
+                                <Portal width={30} aspectRatio={1}>
+                                    <ambientLight intensity={2} />
+                                    <Book
+                                        scale={3}
+                                        rotation={[Math.PI / 2, 0, 0]}
+                                        position={[0, -0.2, 0]}
+                                    />
+                                </Portal>
+                            </Container>
+                        </Button>
+                        <Button variant="outline">
+                            <Container padding={0.2} gap={5}>
+                                <UiText>Contact me</UiText>
+                                <Mails />
+                            </Container>
+                        </Button>
+                    </Container>
+                </Hud>
             </Canvas>
         </div>
     );
