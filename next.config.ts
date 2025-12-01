9f4167c933e766b20237ccf1168dedbf296c3022
christian1koch <ko.che@web.de>
2025-12-01 11:32:40 +0100

    Update next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	transpilePackages: ["three"],
+    transpilePackages: [
+        "three",
+        "@react-three/uikit",
+        "@react-three/uikit-default",
+        "@react-three/uikit-lucide",
+        "@pmndrs/uikit",
+    ],
 };
 
 export default nextConfig;
